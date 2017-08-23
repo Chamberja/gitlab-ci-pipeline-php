c6c237177414b5c604b4cb695533182c6cbefa12
commadore <mradford@gmail.com>
2017-08-23 09:37:00 +0100

    Update node.sh

--- scripts/node.sh
+++ scripts/node.sh
@@ -5,5 +5,4 @@ curl -sL https://deb.nodesource.com/setup_8.x | bash - \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs --no-install-recommends -yqq \
     && npm install -g npm \
     && npm install -g gulp \
-    && npm install -g semantic-release-gitlab \
     && curl -o- -L https://yarnpkg.com/install.sh | bash
